51415887c3e0fbc6ee8f91e150b953923bbc0136
alu0100821390 <alu0100821390@ull.edu.es>
2017-05-08 11:14:43 +0100

    Añadir pruebas mocha chai

---
+++ tests/tests.js
@@ -0,0 +1,19 @@
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F to C = 0C', function() {
+        original.value = "32F to C";
+        main();
+        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    });
+    test('45C to F = 113.0 Fahrenheit', function() {
+        original.value = "45C to F";
+        main();
+        assert.deepEqual(converted.innerHTML, "113.0 Fahrenheit");
+    });
+    test('5X = error', function() {
+        original.value = "5X";
+        main();
+        assert.match(converted.innerHTML, /ERROR/);
+    });
+});
